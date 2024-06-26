6a38406b3230a026bc6c1b86c6314cd473345b1f
beaucoburn <douglas.coburn@gmail.com>
2024-06-26 10:26:59 -0600

    Changes to be committed: 	new file:   TwoParam.class 	new file:   TwoParam.java

---
+++ TwoParam.java
@@ -0,0 +1,9 @@
+public class TwoParam{
+	static int myMethod(int x, int y){
+		return x + y;
+	}
+	public static void main(String[] args){
+		System.out.println(myMethod(5, 3));
+	}
+}
+//Outputs 8 (5+3)
