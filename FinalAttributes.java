7423d3ac25bbdfa87b45bf2520d362019db06092
beaucoburn <douglas.coburn@gmail.com>
2024-07-06 11:24:19 -0600

    new file:   FinalAttributes.java

---
+++ FinalAttributes.java
@@ -0,0 +1,11 @@
+public class FinalAttributes {
+  final int x = 10;
+  final double PI = 3.14;
+
+  public static void main(String[] args) {
+    FinalAttributes myObj = new FinalAttributes();
+    myObj.x = 50;  // will generate an error: cannot assign a value to a final variable
+    myObj.PI = 25;  // will generate an error: cannot assign a value to a final variable
+    System.out.println(myObj.x);
+  }
+}
