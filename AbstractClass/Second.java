6a93f5ea6127dc5a8f89a0c1fffd81d0bbbd727a
beaucoburn <douglas.coburn@gmail.com>
2024-07-06 12:25:38 -0600

    new file:   AbstractClass/Main.class 	new file:   AbstractClass/Main.java 	new file:   AbstractClass/Second.class 	new file:   AbstractClass/Second.java 	new file:   AbstractClass/Student.class

---
+++ AbstractClass/Second.java
@@ -0,0 +1,11 @@
+class Second {
+  public static void main(String[] args) {
+    // create an object of the Student class (which inherits attributes and methods from Main)
+    Student myObj = new Student();
+
+    System.out.println("Name: " + myObj.fname);
+    System.out.println("Age: " + myObj.age);
+    System.out.println("Graduation Year: " + myObj.graduationYear);
+    myObj.study(); // call abstract method
+  }
+}
