2ff56b7e0e5048a981bc6a7f1c72ac8d921b0fa9
beaucoburn <douglas.coburn@gmail.com>
2024-07-09 12:56:41 -0600

    new file:   Encapsulation/Main.class 	new file:   Encapsulation/Main.java 	new file:   Encapsulation/Person.class 	new file:   Encapsulation/Person.java

---
+++ Encapsulation/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    Person myObj = new Person();
+    myObj.setName("John");//Set the value of the name variable to "John"
+    System.out.println(myObj.getName());
+  }
+}
+
+// Outputs "John"
