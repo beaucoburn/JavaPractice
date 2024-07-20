de390bbde27e553795302fadaadecacdfd74f24e
beaucoburn <douglas.coburn@gmail.com>
2024-07-20 10:24:11 -0600

    new file:   ThreadRunnable.class 	new file:   ThreadRunnable.java

---
+++ Threads/ThreadRunnable.java
@@ -0,0 +1,12 @@
+public class ThreadRunnable implements Runnable {
+  public static void main(String[] args) {
+    ThreadRunnable obj = new ThreadRunnable();
+    Thread thread = new Thread(obj);
+    thread.start();
+    System.out.println("This code is outside of the thread");
+  }
+
+  public void run() {
+    System.out.println("This code is running in a thread");
+  }
+}
